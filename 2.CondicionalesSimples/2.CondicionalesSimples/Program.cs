2422f95176335729002f8f06608de87abe1d99d4
B09S202est <B09S202est@MEDAULAS.UPB>
2026-02-18 09:16:43 -0500

    Condicional Simple

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre=Console.ReadLine();//Todo lo que el usuario ingresa desde la consola llega como una cadena de caracteres
+            Console.WriteLine("Ingrese el valor del sueldo");
+            sueldo=Convert.ToSingle( Console.ReadLine() );
+
+            if (sueldo>=3000)
+            {
+                Console.WriteLine($"El usuario {nombre}, debe abonar impuestos");
+            }
+
+        }
+    }
+}
