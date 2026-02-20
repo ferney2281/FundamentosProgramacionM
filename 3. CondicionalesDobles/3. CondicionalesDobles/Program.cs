9cf028f882f818bdb378b9cf96e3be91416b8c10
ferney2281 <ferney.chica@upb.edu.co>
2026-02-20 09:29:32 -0500

    Condicionales dobles o compuestos
    Commit summary
    Condicionales dobles o compuestos
    Optional extended description

---
+++ 3. CondicionalesDobles/3. CondicionalesDobles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.CondicionalesDobles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
+
+            /*string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Ingrese el valor del sueldo");
+            sueldo =Single.Parse(Console.ReadLine());//Convertir a float utilizando la clase Single
+
+            if (sueldo>=3000)
+            {
+                //Cuando la condición es VERDADERA
+                Console.WriteLine($"La persona {nombre} debe abonar impuestos");
+            }
+            else
+            {
+                //Cuando la condición es FALSA
+                Console.WriteLine("Persona exenta de impuestos");
+            }*/
+
+        }
+    }
+}
