db68afd7e4f12d1e94251ffbd494be39aaaaf2b6
ferney2281 <ferney.chica@upb.edu.co>
2026-03-11 09:31:41 -0500

    Ciclo While
    Ciclo While

---
+++ 9. CicloWhile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 0;
+
+            while (contador <=5)
+            {
+                acumulador += contador;
+                contador++;
+
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");*/
+
+            /*Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)*/
+
+            int contador = 1;
+            int acumulador = 1;
+            int numero = 0;
+
+            Console.WriteLine("Ingrese un número para calcular su factorial");
+            numero=Int32.Parse(Console.ReadLine());
+
+            while (contador<=numero)
+            {
+                acumulador*=contador;
+                contador++;
+            }
+            Console.WriteLine($"La factorial del número {numero} es: {acumulador}");
+        }
+    }
+}
